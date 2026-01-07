556c1e605975efebc10f88b0090411ded79210f1
Nishanth Rajesh <nishanth13.rajesh@gmail.com>
2026-01-08 00:57:27 +0530

    Implement daily new summary

--- types/global.d.ts
+++ types/global.d.ts
@@ -215,6 +215,16 @@ declare global {
         threshold: number;
         changePercent?: number;
     };
+
+    type UserForNewsEmail = {
+        id: string;
+        email: string;
+        name: string;
+        country: string;
+        investmentGoals?: string;
+        riskTolerance?: string;
+        preferredIndustry?: string;
+    };
 }
 
 export {};
